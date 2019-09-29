b7c25584fb93aa5b4cd793db41697aa364fa9c35
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2019-09-29 16:26:30 +0100

    Correct instructions

--- src/instructions.js
+++ src/instructions.js
@@ -2,23 +2,22 @@ import React from 'react'
 
 const Instructions = () => (
   <div>
-    <h3>Instructions</h3>
     <p>
-      In this task we are interested in how students think "scientifically" in a simulated experimental setting. You
+      In this task we are interested in how people think "scientifically" in a simulated experimental setting. You
       will be given
-      an initial observation, three numbers which conform to a simple rule that I have in mind. Your task is to discover
+      an initial observation, three numbers which conform to a simple rule. Your task is to discover
       this rule
       by generating a possible rule (a hypothesis) and testing it with
       your own set of three numbers.
     </p>
     <p>
-      Write down your guess about the rule together with your test
-      triple of numbers on the record sheet. The only information I
-      will provide is an indication on your record sheet whether or
-      not your test triple conforms or does not conform to my rule.
+      Enter your guess about the rule together with your test
+      triple of numbers below. The only information that will be
+      provided is an indication of whether or
+      not your test triple conforms or does not conform to the rule.
     </p>
     <p>
-      My rule is concerned with a RELATION between any three
+      The rule is concerned with a RELATION between any three
       numbers and not with their absolute magnitude; so it is not a
       rule like "all numbers above (or below) 50", etc. Keep in mind
       that this is an arbitrary rule, one from an infinite set of rules
@@ -28,7 +27,7 @@ const Instructions = () => (
       You may continue as long as you wish to propose guesses
       and tests of them with number triples. Continue until you have
       proven to yourself that you know what the rule is. Only then,
-      AND NOT BEFORE, are you to write down this rule and CIRCLE it on your record sheet.
+      AND NOT BEFORE, state this rule at the bottom.
       Remember that the instruction "to prove" means to show that
       your rule is the only true one.
     </p>
